f121606023daf91c5381022caa8a05c909fef733
Vladyslav Nahornyi <vladyslav.nahornyi.dev@gmail.com>
2025-09-28 09:46:32 +0300

    added dotenv

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({
+  path: path.resolve(__dirname, "../../.env"),
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
 };
 
 export default nextConfig;
